a0a03b65203d543fcd3d6193c428553e1419b86e
Janusz Witkowski <46355383+JanuszWitkowski@users.noreply.github.com>
2023-04-16 12:34:03 +0200

    Fix another part of the objective function

--- lab2/1-tartak.jl
+++ lab2/1-tartak.jl
@@ -1,8 +1,8 @@
 using JuMP
 using HiGHS
 
-function unused(x, cuts, demand, n_of_cuts, n_of_widths)
-    return sum(sum(cuts[i, j] * x[i] for i in 1:n_of_cuts) - demand[j] for j in 1:n_of_widths)
+function unused(x, cuts, demand, widths, n_of_cuts, n_of_widths)
+    return sum((sum(cuts[i, j] * x[i] for i in 1:n_of_cuts) - demand[j]) * widths[j] for j in 1:n_of_widths)
 end
 
 function leftovers(x, main_width, widths, cuts, n_of_cuts, n_of_widths)
@@ -18,20 +18,21 @@ cuts = [3 0 0; 2 1 1; 2 0 2; 1 3 0; 1 2 1; 1 1 3; 1 0 5; 0 4 0; 0 3 2; 0 2 4; 0 
 
 model = Model(HiGHS.Optimizer)
 @variable(model, 0 <= x[i = 1:n_of_cuts], integer=true)
-# @objective(model, Min, sum(sum(cuts[i, j] * x[i] for i in 1:n_of_cuts) - demand[j] for j in 1:n_of_widths) + sum(x[i] * (main_width - sum(widths[j] * cuts[i, j] for j in 1:n_of_widths)) for i in 1:n_of_cuts))
-@objective(model, Min, unused(x, cuts, demand, n_of_cuts, n_of_widths) + leftovers(x, main_width, widths, cuts, n_of_cuts, n_of_widths))
+@objective(model, Min, unused(x, cuts, demand, widths, n_of_cuts, n_of_widths) + leftovers(x, main_width, widths, cuts, n_of_cuts, n_of_widths))
 for i in 1:n_of_cuts
+    # constraint_name = string("good_cut_", i)
     @constraint(model, sum(cuts[i, j] for j in 1:n_of_widths) <= main_width)        # These have to be good cuts
 end
 for j in 1:n_of_widths
+    # constraint_name = string("supply_demand_", j)
     @constraint(model, sum(cuts[i, j] * x[i] for i in 1:n_of_cuts) >= demand[j])    # Supply the demand
 end
 println(model)
 optimize!(model)
 termination_status(model)
 println("Objective value: ", objective_value(model))
-# JuMP.value.(x)
+JuMP.value.(x)
 println("x: ", JuMP.value.(x))
 x = JuMP.value.(x)
-println("Niewykorzystane: ", unused(x, cuts, demand, n_of_cuts, n_of_widths))
+println("Niewykorzystane: ", unused(x, cuts, demand, widths, n_of_cuts, n_of_widths))
 println("Resztki: ", leftovers(x, main_width, widths, cuts, n_of_cuts, n_of_widths))
