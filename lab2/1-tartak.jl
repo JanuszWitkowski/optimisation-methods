e6be11fe3e4a98d18459655eefb28ffadffe4977
Janusz Witkowski <janusz.witkowski@vewd.com>
2023-04-21 17:27:24 +0200

    Write a printing function for Task 2

--- lab2/1-tartak.jl
+++ lab2/1-tartak.jl
@@ -1,5 +1,6 @@
 using JuMP
-using HiGHS
+using GLPK
+# using Cbc
 
 function unused(x, cuts, demand, widths, n_of_cuts, n_of_widths)
     return sum((sum(cuts[i, j] * x[i] for i in 1:n_of_cuts) - demand[j]) * widths[j] for j in 1:n_of_widths)
@@ -16,7 +17,8 @@ demand = [110 120 80]
 n_of_cuts = 12
 cuts = [3 0 0; 2 1 1; 2 0 2; 1 3 0; 1 2 1; 1 1 3; 1 0 5; 0 4 0; 0 3 2; 0 2 4; 0 1 5; 0 0 7]
 
-model = Model(HiGHS.Optimizer)
+model = Model(GLPK.Optimizer)
+# model = Model(Cbc.Optimizer)
 @variable(model, 0 <= x[i = 1:n_of_cuts], integer=true)
 @objective(model, Min, unused(x, cuts, demand, widths, n_of_cuts, n_of_widths) + leftovers(x, main_width, widths, cuts, n_of_cuts, n_of_widths))
 for i in 1:n_of_cuts
--- lab2/2-maszyna.jl
+++ lab2/2-maszyna.jl
@@ -1,21 +1,81 @@
 using JuMP
-using HiGHS
+using GLPK
+# using Cbc
+
+function end_begin_str(job, jobs, durations, c, is_begin)
+    if is_begin
+        for j in jobs
+            if job != j
+                if c[j] == c[job] - durations[job]
+                    return "|"
+                end
+            end
+        end
+        return "["
+    else
+        for j in jobs
+            if job != j
+                if c[job] == c[j] - durations[j]
+                    return " "
+                end
+            end
+        end
+        return "]"
+    end
+end
+
+function print_job_solution(index, jobs, durations, c)
+    c_max = maximum(c)
+    print("M", index, "\t|")
+    for c_i in 0:c_max
+        # tmp = j
+        # modulus = 0
+        # while tmp > 0
+        #     modulus += 1
+        #     tmp /= 10
+        # end
+        print_space = true
+        for j in jobs
+            if c_i == c[j] - durations[j]
+                # print("[", j)
+                print(end_begin_str(j, jobs, durations, c, true), j)
+                print_space = false
+            elseif c_i == c[j]
+                # print("]")
+                print(end_begin_str(j, jobs, durations, c, false), " ")
+                print_space = false
+            elseif c_i > c[j] - durations[j] && c_i < c[j]
+                print("  ")
+                print_space = false
+                # println(c_i)
+            # else
+            #     print("-")
+            end
+        end
+        if print_space
+            print("  ")
+        end
+    end
+    println()
+end
 
 function job_flow(jobs, durations, weights, ready)
-    model = Model(HiGHS.Optimizer)
+    model = Model(GLPK.Optimizer)
+    # model = Model(Cbc.Optimizer)
     @variable(model, 0 <= c[j = 1:jobs], Int)
     @objective(model, Min, sum(weights[j] * c[j] for j in 1:jobs))
     for j in 1:jobs
         @constraint(model, c[j] - durations[j] >= ready[j])     # Do not start a job before it's ready.
     end
-    for j in 1:jobs
-        for i in 1:jobs
-            # @constraint(model, (i == j) || ((c[j] <= c[i] && c[j] <= c[i] - durations[i]) || (c[j] >= c[i] && c[j] >= c[i] - durations[i])))
-            if i != j
-                @constraint(model, ((c[j] <= c[i] && c[j] <= c[i] - durations[i]) + (c[j] >= c[i] && c[j] >= c[i] - durations[i])) == 1)
-            end
-        end
-    end
+    # for j in 1:jobs
+    #     for i in 1:jobs
+    #         # @constraint(model, (i == j) || ((c[j] <= c[i] && c[j] <= c[i] - durations[i]) || (c[j] >= c[i] && c[j] >= c[i] - durations[i])))
+    #         if i != j
+    #             # @constraint(model, ((c[j] <= c[i] && c[j] <= c[i] - durations[i]) + (c[j] >= c[i] && c[j] >= c[i] - durations[i])) == 1)
+    #             @constraint(model, (c[j] - c[i]) * (c[j] - c[i] + durations[i]) >= 0)   # NON-LINEAR!!
+    #         end
+    #     end
+    # end
     println(model)
     optimize!(model)
 
@@ -36,5 +96,7 @@ durations = [24 36 13]
 weights = [17 18 19]
 ready = [12 23 13]
 
-job_flow(jobs, durations, weights, ready)
+# job_flow(jobs, durations, weights, ready)
+
+print_job_solution(1, [1 2 3], [2 4 6], [2 6 15])
 
