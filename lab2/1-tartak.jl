d4323872156b7cf286f84c4f7cd48f7695d0fefe
Janusz Witkowski <46355383+JanuszWitkowski@users.noreply.github.com>
2023-04-23 14:45:46 +0200

    Update 1-tartak.jl

--- lab2/1-tartak.jl
+++ lab2/1-tartak.jl
@@ -1,6 +1,28 @@
+#= OPTIMIZATION METHODS
+ex 2.1 - Sawmill
+Author: Janusz Witkowski 254663
+=#
+
 using JuMP
 using GLPK
 # using Cbc
+
+
+function calc_cuts!(cuts, current_cut, current_width, widths, n_of_widths)
+    counter = 0
+    for i in 1:n_of_widths
+        if current_width >= widths[i]
+            counter += 1
+            new_cut = [cut for cut in current_cut]
+            new_cut[i] += 1
+            new_width = current_width - widths[i]
+            calc_cuts!(cuts, new_cut, new_width, widths, n_of_widths)
+        end
+    end
+    if counter == 0
+        push!(cuts, current_cut)
+    end
+end
 
 function unused(x, cuts, demand, widths, n_of_cuts, n_of_widths)
     return sum((sum(cuts[i, j] * x[i] for i in 1:n_of_cuts) - demand[j]) * widths[j] for j in 1:n_of_widths)
@@ -10,12 +32,20 @@ function leftovers(x, main_width, widths, cuts, n_of_cuts, n_of_widths)
     return sum(x[i] * (main_width - sum(widths[j] * cuts[i, j] for j in 1:n_of_widths)) for i in 1:n_of_cuts)
 end
 
+
+# PARAMETERS GIVEN
 main_width = 22
-n_of_widths = 3
 widths = [7 5 3]
 demand = [110 120 80]
-n_of_cuts = 12
-cuts = [3 0 0; 2 1 1; 2 0 2; 1 3 0; 1 2 1; 1 1 3; 1 0 5; 0 4 0; 0 3 2; 0 2 4; 0 1 5; 0 0 7]
+
+# PARAMETERS CALCULATED
+n_of_widths = length(widths)
+cuts = []
+calc_cuts!(cuts, [0, 0, 0], main_width, widths, n_of_widths)
+cuts = unique(cuts)
+n_of_cuts = length(cuts)
+cuts = mapreduce(permutedims, vcat, cuts)
+
 
 model = Model(GLPK.Optimizer)
 # model = Model(Cbc.Optimizer)
@@ -31,10 +61,12 @@ for j in 1:n_of_widths
 end
 println(model)
 optimize!(model)
+
 termination_status(model)
 println("Objective value: ", objective_value(model))
-JuMP.value.(x)
-println("x: ", JuMP.value.(x))
 x = JuMP.value.(x)
-println("Niewykorzystane: ", unused(x, cuts, demand, widths, n_of_cuts, n_of_widths))
-println("Resztki: ", leftovers(x, main_width, widths, cuts, n_of_cuts, n_of_widths))
+println("Solution: ", x)
+println("Possible cuts: ", cuts)
+println("Unused: ", unused(x, cuts, demand, widths, n_of_cuts, n_of_widths))
+println("Leftovers: ", leftovers(x, main_width, widths, cuts, n_of_cuts, n_of_widths))
+
