90ab45ce65b8527ccb37fdaaa7b4f691cc73b142
Janusz Witkowski <janusz.witkowski@vewd.com>
2023-06-09 11:13:29 +0200

    Download instances of SUPMMC

---
+++ lab3/supmmc.jl
@@ -0,0 +1,10 @@
+#= OPTIMIZATION METHODS
+ex 3 - Scheduling on Unrelated Parallel Machines and Makespan Criterion
+Author: Janusz Witkowski 254663
+=#
+
+using JuMP
+import GLPK
+import Cbc
+import CPLEX
+
