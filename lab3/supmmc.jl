2d8096257758b4838606a77bd8360c4309162916
Janusz Witkowski <janusz.witkowski@vewd.com>
2023-06-09 13:51:57 +0200

    Implement 2-approx algorithm

--- lab3/supmmc.jl
+++ lab3/supmmc.jl
@@ -4,7 +4,107 @@ Author: Janusz Witkowski 254663
 =#
 
 using JuMP
-import GLPK
+# import GLPK
 import Cbc
-import CPLEX
+# import CPLEX
+
+
+function read_file(filename)
+    lines = open(filename) do file
+        readlines(file)
+    end
+    sizes = parse.(Int,split(replace(lines[1], r"[^0-9.]"=>" ")))
+    n, m = sizes[1], sizes[2]
+    # println(n ," - ", m)
+    times = zeros(Int, n, m)
+    for (i,line) in enumerate(lines[3:end])
+        parsed = parse.(Int,split(replace(line, r"[^0-9.]"=>" ")))
+        times[i,:] = parsed[2:2:end]
+    end
+    return times
+end
+
+
+function greedy_schedule(times)
+    (n, m) = size(times)
+    makespans = zeros(Int64, m)
+
+    for j in 1:n
+        minimum, machine = findmin(times[j,:])
+        makespans[machine] += minimum
+    end
+
+    return maximum(makespans)
+end
+
+
+function check_feasibility(times, T)
+    (n, m) = size(times)
+    Jobs = 1:n
+    Machines = 1:m
+    S_j = [[] for _ in Jobs]
+    S_i = [[] for _ in Machines]
+    for j in Jobs
+        for i in Machines
+            if times[j,i] <= T
+                append!(S_j[j], i)
+                append!(S_i[i], j)
+            end
+        end
+    end
+
+    # model = Model(GLPK.Optimizer)
+    model = Model(Cbc.Optimizer)
+    # model = Model(CPLEX.Optimizer)
+
+    @variable(model, x[Jobs, Machines] >= 0)
+    @constraint(model, [j in Jobs], sum(x[j,i] for i in S_j[j]) == 1)
+    @constraint(model, [i in Machines], sum(x[j,i] * times[j,i] for j in S_i[i]) <= T)
+    @objective(model, Min, 0)
+    optimize!(model)
+    if termination_status(model) == OPTIMAL::TerminationStatusCode
+        return true, value.(x)
+    end
+    return false, nothing
+end
+
+
+function binary_search(times, range)
+    (n, m) = size(times)
+    l = range ÷ m
+    r = range
+    mid = 0
+    result = nothing
+    while l < r
+        mid = (l+r) ÷ 2
+        feasible, result = check_feasibility(times, mid)
+        if feasible
+            r = mid - 1
+        else
+            l = mid + 1
+        end
+    end
+    return mid + 1, result
+end
+
+
+
+
+DEFAULT_FILENAME = "RCmax/instancias100a120/111.txt"
+
+filename = DEFAULT_FILENAME
+if length(ARGS) > 0
+    filename = ARGS[1]
+end
+println("Instance: ", filename)
+p = read_file(filename)
+# display(p)
+alpha = greedy_schedule(p)
+println("Alpha: ", alpha)
+T_min, result = binary_search(p, alpha)
+feasibility, _ = check_feasibility(p, T_min)
+display(result)
+println("Value of objective function for instance ", filename)
+println("T_min = ", T_min)
+println("Feasibility: ", feasibility)
 
